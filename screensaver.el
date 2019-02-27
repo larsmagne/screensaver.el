2b382641f60989acfebbedb387ea083fb9eab92e
Lars Ingebrigtsen <larsi@gnus.org>
2019-02-27 15:33:08 +0100

    Scaffolding

---
+++ screensaver.el
@@ -0,0 +1,28 @@
+;;; screensaver.el --- an Emacs-based screensaver for X -*- lexical-binding: t -*-
+;; Copyright (C) 2019 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: extensions, processes
+
+;; screensaver.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; screensaver.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'screensaver)
+
+;;; screensaver.el ends here
