c134ceb4700de9f9c0002346a73766b6b15488d4
Lars Ingebrigtsen <larsi@gnus.org>
2019-02-27 16:05:18 +0100

    Start implementation

--- screensaver.el
+++ screensaver.el
@@ -23,6 +23,73 @@
 
 ;;; Code:
 
+(defvar screensaver--timer nil)
+(defvar screensaver--timeout (* 5 60))
+(defvar screensaver--action nil)
+
+(defun screensaver-start (&optional timeout action)
+  "Start saving the screen after TIMEOUT seconds.
+Perform ACTION when saving the screen.  The default is to display
+a blank Emacs frame."
+  (interactive)
+  (unless (executable-find "xssstate")
+    (error "The xssstate executable is not installed on this system"))
+  (screensaver-stop)
+  (when timeout
+    (setq screensaver--timeout timeout))
+  (setq screensaver--action action)
+  (setq screensaver-timer (screensaver--schedule)))
+
+(defun screensaver-stop ()
+  "Stop saving the screen."
+  (interactive)
+  (when screensaver--timer
+    (cancel-timer screensaver--timer)
+    (setq screensaver--timer nil)))
+
+(defun screensaver-activate ()
+  "Activate the screensaver immediately."
+  (interactive)
+  (screensaver--activate))
+
+(defun screensaver--schedule ()
+  "Compute the first likely time that screen saving can happen."
+  (let ((idle (with-temp-buffer
+		(call-process "xssstate" nil t nil "-i")
+		(string-to-number (buffer-string)))))
+    (if (>= (/ idle 1000) screensaver--timeout)
+	;; We've already been activated.
+	(unless (get-buffer "*screensaver*")
+	  (screensaver--activate))
+      (setq screensaver--timer
+	    (run-at-time (- screensaver--timeout (/ idle 1000)) nil
+			 (lambda ()
+			   (screensaver--schedule)))))))
+
+(defun screensaver--activate ()
+  (let ((selected (selected-frame))
+	(frame
+	 (make-frame
+	  `((title . "Screensaver")
+	    (left . 0)
+	    (top . 0)
+	    (width . 400)
+	    (height . 200)
+	    (user-position . t)
+	    (background-color . "blue")
+	    (vertical-scroll-bars . nil)))))
+    (select-frame-set-input-focus frame)
+    (let ((buffer (switch-to-buffer "*screensaver*")))
+      (erase-buffer)
+      (when screensaver--action
+	(funcall screensaver--action))
+      ;; Wait until we get some event (mouse movement, keyboard action).
+      (track-mouse (read-event))
+      (delete-frame frame)
+      (kill-buffer buffer)
+      (screensaver--schedule)
+      (select-frame-set-input-focus selected))))
+
 (provide 'screensaver)
 
 ;;; screensaver.el ends here
